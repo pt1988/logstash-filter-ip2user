e58a756b8a4b44bf44c72b38e92499257d2e9da2
pt1988 <pt1988@gmail.com>
2016-03-29 12:09:01 +0700

    add function

--- lib/logstash/filters/ip2user.rb
+++ lib/logstash/filters/ip2user.rb
@@ -20,7 +20,11 @@ class LogStash::Filters::Ip2user < LogStash::Filters::Base
   milestone 1
 
   # Replace the message with this value.
-  config :message, :validate => :string
+  config :action, :validate => [ "add", "remove", "getuser", "getip"], :default => ""
+  config :debug, :validate => [ "true", "false"], :default => "false"
+  config :ipv4, :validate => :string, :default => "ipv4"
+  config :ipv6, :validate => :string, :default => "ipv6"
+  config :username, :validate => :string, :default => "username"
 
   public
   def register
@@ -29,40 +33,52 @@ class LogStash::Filters::Ip2user < LogStash::Filters::Base
 
   public
   def filter(event)
-    # return nothing unless there's an actual filter event
-    return unless filter?(event)
-   #   system("echo \"enter ip2user plugin\"")
-    if event["userStatus"] and event["userID"] and event["program"] == "Firewall-Auth" 
-   #   system("echo \"enter ip2user=>check userStatus,userID,program\"")
-      #check ipv4 parameter is exist?
-      if event["userIPv4"] and event["userIPv4"] != "-"  
-        if event["userStatus"] == "allow"
-          $ipv4[event["userIPv4"]]=event["userID"]
-        elsif event["userStatus"] == "remove" 
-          $ipv4.delete(event["userIPv4"])
-        end
- #       event["ip2userv4_size"] = $ipv4.length
-      end 
-      #check ipv6 parameter is exist?
-      if event["userIPv6"] and event["userIPv6"] != "-"
-        if event["userStatus"] == "allow"
-          $ipv6[event["userIPv6"]]=event["userID"]
-        elsif event["userStatus"] == "remove"
-          $ipv6.delete(event["userIPv6"])
-        end
- #       event["ip2userv6_size"] = $ipv6.length 
-      end
-      ipaddress_status="ipv4.length #{$ipv4.length}\nipv6.length #{$ipv6.length}"
-  #    system("echo \"#{ipaddress_status}\" > \/dev\/shm\/logstash_ip")
-    end
+	# return nothing unless there's an actual filter event
+	return unless filter?(event)
+	#   system("echo \"enter ip2user plugin\"")
+	# if event["userStatus"] and event["userID"] and event["program"] == "Firewall-Auth" 
+	if @action == "add" or @action == "remove"
+		if event[@ipv4] or event[event[@ipv6]]    
+			#   system("echo \"enter ip2user=>check userStatus,userID,program\"")
+			#check ipv4 parameter is exist?
+			if event[@ipv4] != "-"  
+				if @action == "add"
+					$ipv4[event[@ipv4]]=event[@username]
+				elsif @action == "remove" 
+					$ipv4.delete(event[@ipv4])
+				end
+				#       event["ip2userv4_size"] = $ipv4.length
+			end 
+			#check ipv6 parameter is exist?
+			if event[@ipv6] != "-"
+				if @action == "add"
+				  $ipv6[event[@ipv6]]=event[@username]
+				elsif @action == "remove"
+				  $ipv6.delete(event[@ipv6])
+				end
+				#       event["ip2userv6_size"] = $ipv6.length 
+			end
+			ipaddress_status="ipv4.length #{$ipv4.length}\nipv6.length #{$ipv6.length}"
+			system("echo \"#{ipaddress_status}\" > \/dev\/shm\/logstash_ip")
+		end
 
 
-    if ! event["userID"] and event["userIPv4"] and event['userIPv4'] != "-" and $ipv4[event["userIPv4"]]
-       event["userID"] = $ipv4[event["userIPv4"]]
+	elsif @action == "getuser" and event[@ipv4] != "-" and $ipv4[event[@ipv4]]
+		event["userID"] = $ipv4[event[@ipv4]]
+
+	elsif @action == "getuser" and event[@ipv6] != "-" and $ipv6[event[@ipv6]]
+		event["userID"] = $ipv6[event[@ipv6]]
+
+	elsif @action == "getip" and event[@username] != nil 
+
+	end	
+	if @debug == "true"
+		puts $ipv4	
+		puts $ipv6	
+	end	
 #	@@mapping_counter+=1	
    #    system("echo \"#{@@mapping_counter}\">/dev/shm/mapping_counter")
    #    system("echo \"evnet mapping ip complete #{event["userID"]}<=>#{event['userIPv4']}\"")
-    end
     #if @message
       # Replace the event message with our message as configured in the
       # config file.
